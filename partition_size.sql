9393e3db9e49fccaae7815f9274ca47575c965c6
steveandreassend <119098920+steveandreassend@users.noreply.github.com>
2023-11-21 15:36:12 +0100

    Update partition_size.sql

--- partition_size.sql
+++ partition_size.sql
@@ -1,47 +1,5 @@
 set lines 400 pages 10000
 
-/* DEMO DATA
-
-PROMPT Create some test tables with range and composite partitioning
-
-CREATE TABLE PRODUCTS (partno NUMBER,
-      description VARCHAR(32),
-       costprice NUMBER)
-  PARTITION BY RANGE (partno)
-     SUBPARTITION BY HASH(description)
-    SUBPARTITIONS 8
-      (PARTITION p1 VALUES LESS THAN (100),
-       PARTITION p2 VALUES LESS THAN (200),
-       PARTITION p3 VALUES LESS THAN (MAXVALUE));
-
-CREATE INDEX ind_products on PRODUCTS (partno) LOCAL;
-
-INSERT INTO PRODUCTS VALUES (1,1,1);
-INSERT INTO PRODUCTS VALUES (101,1,1);
-INSERT INTO PRODUCTS VALUES (201,1,1);
-COMMIT;
-
-CREATE TABLE SALES (
-    sales_id NUMBER,
-    sales_date DATE,
-    amount NUMBER
-)
-PARTITION BY RANGE (sales_date) (
-    PARTITION sales_q1_2023 VALUES LESS THAN (TO_DATE('2023-04-01', 'YYYY-MM-DD')),
-    PARTITION sales_q2_2023 VALUES LESS THAN (TO_DATE('2023-07-01', 'YYYY-MM-DD')),
-    PARTITION sales_q3_2023 VALUES LESS THAN (TO_DATE('2023-10-01', 'YYYY-MM-DD')),
-    PARTITION sales_q4_2023 VALUES LESS THAN (TO_DATE('2024-01-01', 'YYYY-MM-DD'))
-);
-
-CREATE INDEX ind_SALES on SALES (sales_id) LOCAL;
-
-INSERT INTO SALES VALUES (1,TO_DATE('2023-04-01', 'YYYY-MM-DD')-1,100);
-INSERT INTO SALES VALUES (1,TO_DATE('2023-07-01', 'YYYY-MM-DD')-1,100);
-INSERT INTO SALES VALUES (1,TO_DATE('2023-10-01', 'YYYY-MM-DD')-1,100);
-INSERT INTO SALES VALUES (1,TO_DATE('2024-01-01', 'YYYY-MM-DD')-1,100);
-COMMIT;
-*/
-
 col owner FORMAT A30
 col table_owner FORMAT A30
 col index_owner FORMAT A30
@@ -152,10 +110,3 @@ from dba_ind_subpartitions b
 where b.index_owner IN (SELECT username FROM dba_users where oracle_maintained = 'N') 
 group by b.index_owner, b.index_name, b.partition_name
 order by 1,2;
-
-/* DROP DEMO DATA
-
-DROP TABLE SALES;
-DROP TABLE PRODUCTS;
-
-*/
