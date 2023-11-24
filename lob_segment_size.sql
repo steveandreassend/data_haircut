b37b9dc40475a1f49b7e5ac392ab7e7f8148d538
steveandreassend <119098920+steveandreassend@users.noreply.github.com>
2023-11-24 14:07:18 +0100

    Update lob_segment_size.sql

--- lob_segment_size.sql
+++ lob_segment_size.sql
@@ -1,5 +1,22 @@
 set lines 400 pages 10000
 
+col owner FORMAT A30
+col table_owner FORMAT A30
+col index_owner FORMAT A30
+col table_name FORMAT A30
+col index_name FORMAT A30
+col partition_name FORMAT A30
+col tablespace_name FORMAT A30
+col segment_name FORMAT A30
+col segment_type FORMAT A30
+col DEF_TAB_COMPRESSION FORMAT A16
+col COMPRESS_FOR FORMAT A16
+col BIGFILE FORMAT A10
+col status FORMAT A12
+col size_in_gb FORMAT 999,999,999,999.00
+col num_tablespaces FORMAT 999,999,999,999
+col num_partitions FORMAT 999,999,999,999
+
 PROMPT Report LOB Segments Storage Size
 
 select s.owner, l.table_name, l.column_name, l.tablespace_name, l.securefile, l.compression, s.segment_name, s.segment_type, ROUND(SUM(s.bytes) / (1024 * 1024 * 1024), 2) AS size_in_gb
