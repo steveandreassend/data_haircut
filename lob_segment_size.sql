62c8b23cf3f88389aeb25c188876dcc911fb4620
steveandreassend <119098920+steveandreassend@users.noreply.github.com>
2023-11-24 14:18:40 +0100

    Update lob_segment_size.sql

--- lob_segment_size.sql
+++ lob_segment_size.sql
@@ -21,7 +21,7 @@ PROMPT Report LOB Segments Storage Size
 
 select s.owner, l.table_name, l.column_name, l.tablespace_name, l.securefile, l.compression, s.segment_name, s.segment_type, ROUND(SUM(s.bytes) / (1024 * 1024 * 1024), 2) AS size_in_gb
 from dba_lobs l, dba_segments s
-where s.segment_type = 'LOBSEGMENT'
+where s.segment_type LIKE '%LOB%'
 and s.owner IN (SELECT username FROM dba_users where oracle_maintained = 'N') 
 AND s.owner = l.owner
 and s.segment_name = l.segment_name
