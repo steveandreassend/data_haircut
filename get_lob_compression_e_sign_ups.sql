39743d873b7ccd28e108c3ccdc198c83731b9498
steveandreassend <119098920+steveandreassend@users.noreply.github.com>
2023-11-29 21:56:54 +0100

    Update get_lob_compression_e_sign_ups.sql

--- get_lob_compression_e_sign_ups.sql
+++ get_lob_compression_e_sign_ups.sql
@@ -1,125 +1,51 @@
 SET LINES 400 PAGES 1000
-set serveroutput on
-
-PROMPT Get LOW LOB COMPRESSION ratios
+set serveroutput on SIZE 25000
 
 DECLARE
-  l_blkcnt_cmp pls_integer;
-  l_blkcnt_uncmp pls_integer;
-  l_row_cmp pls_integer;
-  l_row_uncmp pls_integer;
-  l_cmp_ratio pls_integer;
-  l_cmptype_str varchar2(100);
+  l_blkcnt_cmp     PLS_INTEGER;
+  l_blkcnt_uncmp   PLS_INTEGER;
+  l_row_cmp        PLS_INTEGER;
+  l_lobcnt         PLS_INTEGER;
+  l_cmp_ratio      NUMBER;
+  l_comptype_str   VARCHAR2(32767);
+
   l_scratchtbsname varchar2(256) := 'USERS';
   l_tabowner varchar2(256) := 'AAX2SW';
   l_tabname varchar2(256) := 'E_SIGN_UPS';
-  l_lobname varchar2(256) := 'SYS_LOB0000405346C00004$$';
-BEGIN
-  DBMS_COMPRESSION.GET_COMPRESSION_RATIO (
-    scratchtbsname => l_scratchtbsname,
-    tabowner => l_tabowner,
-    tabname => l_tabname,
-    lobname => l_lobname,
---    partname => ,
-    comptype => DBMS_COMPRESSION.COMP_LOB_LOW,
-    blkcnt_cmp => l_blkcnt_cmp,
-    blkcnt_uncmp => l_blkcnt_uncmp,
-    row_cmp => l_row_cmp,
-    row_uncmp => l_row_uncmp,
-    cmp_ratio => l_cmp_ratio,
-    comptype_str => l_cmptype_str,
-    subset_numrows => DBMS_COMPRESSION.COMP_RATIO_LOB_MAXROWS /* 5000 rows sampled */
+  l_lobname varchar2(256) := 'XML_PLAIN';
+
+  l_numbers CONSTANT SYS.ODCINUMBERLIST := SYS.ODCINUMBERLIST(
+    DBMS_COMPRESSION.COMP_LOB_LOW,
+    DBMS_COMPRESSION.COMP_LOB_MEDIUM,
+    DBMS_COMPRESSION.COMP_LOB_HIGH
   );
 
-  DBMS_OUTPUT.PUT_LINE('Object = '||l_tabowner||'.'||l_tabname||'.'||l_lobname);
-  DBMS_OUTPUT.PUT_LINE('Compression Type = DBMS_COMPRESSION.COMP_LOB_LOW');  
-  DBMS_OUTPUT.PUT_LINE('Block count compressed = '|| l_blkcnt_cmp);
-  DBMS_OUTPUT.PUT_LINE('Block count uncompressed = '|| l_blkcnt_uncmp);
-  DBMS_OUTPUT.PUT_LINE('Row count per block compressed = '|| l_row_cmp);
-  DBMS_OUTPUT.PUT_LINE('Row count per block uncompressed = '|| l_row_uncmp);
-  DBMS_OUTPUT.PUT_LINE('Compression type = '|| l_cmptype_str);
-  DBMS_OUTPUT.PUT_LINE('Compression ratio = '|| l_cmp_ratio);
-END;
-/
-
-PROMPT Get MEDIUM LOB COMPRESSION ratios...
-
-DECLARE
-  l_blkcnt_cmp pls_integer;
-  l_blkcnt_uncmp pls_integer;
-  l_row_cmp pls_integer;
-  l_row_uncmp pls_integer;
-  l_cmp_ratio pls_integer;
-  l_cmptype_str varchar2(100);
-  l_scratchtbsname varchar2(256) := 'USERS';
-  l_tabowner varchar2(256) := 'AAX2SW';
-  l_tabname varchar2(256) := 'E_SIGN_UPS';
-  l_lobname varchar2(256) := 'SYS_LOB0000405346C00004$$';
 BEGIN
-  DBMS_COMPRESSION.GET_COMPRESSION_RATIO (
-    scratchtbsname => l_scratchtbsname,
-    tabowner => l_tabowner,
-    tabname => l_tabname,
-    lobname => l_lobname,
---    partname => ,
-    comptype => DBMS_COMPRESSION.COMP_LOB_MEDIUM,
-    blkcnt_cmp => l_blkcnt_cmp,
-    blkcnt_uncmp => l_blkcnt_uncmp,
-    row_cmp => l_row_cmp,
-    row_uncmp => l_row_uncmp,
-    cmp_ratio => l_cmp_ratio,
-    comptype_str => l_cmptype_str,
-    subset_numrows => DBMS_COMPRESSION.COMP_RATIO_LOB_MAXROWS /* 5000 rows sampled */
-  );
+  FOR i IN 1..l_numbers.COUNT LOOP
+  -- Loop through different compression types
+    DBMS_COMPRESSION.GET_COMPRESSION_RATIO (
+      scratchtbsname => l_scratchtbsname,
+      tabowner       => l_tabowner,
+      tabname        => l_tabname,
+      lobname        => l_lobname,
+      partname       => NULL,
+      comptype       => l_numbers(i),
+      blkcnt_cmp     => l_blkcnt_cmp,
+      blkcnt_uncmp   => l_blkcnt_uncmp,
+      lobcnt         => l_lobcnt,
+      cmp_ratio      => l_cmp_ratio,
+      comptype_str   => l_comptype_str,
+      subset_numrows => DBMS_COMPRESSION.COMP_RATIO_LOB_MAXROWS /* 5000 rows sampled */
+    );
 
-  DBMS_OUTPUT.PUT_LINE('Object = '||l_tabowner||'.'||l_tabname||'.'||l_lobname);
-  DBMS_OUTPUT.PUT_LINE('Compression Type = DBMS_COMPRESSION.COMP_LOB_MEDIUM');  
-  DBMS_OUTPUT.PUT_LINE('Block count compressed = '|| l_blkcnt_cmp);
-  DBMS_OUTPUT.PUT_LINE('Block count uncompressed = '|| l_blkcnt_uncmp);
-  DBMS_OUTPUT.PUT_LINE('Row count per block compressed = '|| l_row_cmp);
-  DBMS_OUTPUT.PUT_LINE('Row count per block uncompressed = '|| l_row_uncmp);
-  DBMS_OUTPUT.PUT_LINE('Compression type = '|| l_cmptype_str);
-  DBMS_OUTPUT.PUT_LINE('Compression ratio = '|| l_cmp_ratio);
-END;
-/
-
-PROMPT Get HIGH LOB COMPRESSION ratios....
-
-DECLARE
-  l_blkcnt_cmp pls_integer;
-  l_blkcnt_uncmp pls_integer;
-  l_row_cmp pls_integer;
-  l_row_uncmp pls_integer;
-  l_cmp_ratio pls_integer;
-  l_cmptype_str varchar2(100);
-  l_scratchtbsname varchar2(256) := 'USERS';
-  l_tabowner varchar2(256) := 'AAX2SW';
-  l_tabname varchar2(256) := 'E_SIGN_UPS';
-  l_lobname varchar2(256) := 'SYS_LOB0000405346C00004$$';
-BEGIN
-  DBMS_COMPRESSION.GET_COMPRESSION_RATIO (
-    scratchtbsname => l_scratchtbsname,
-    tabowner => l_tabowner,
-    tabname => l_tabname,
-    lobname => l_lobname,
---    partname => ,
-    comptype => DBMS_COMPRESSION.COMP_LOB_HIGH,
-    blkcnt_cmp => l_blkcnt_cmp,
-    blkcnt_uncmp => l_blkcnt_uncmp,
-    row_cmp => l_row_cmp,
-    row_uncmp => l_row_uncmp,
-    cmp_ratio => l_cmp_ratio,
-    comptype_str => l_cmptype_str,
-    subset_numrows => DBMS_COMPRESSION.COMP_RATIO_LOB_MAXROWS /* 5000 rows sampled */
-  );
-
-  DBMS_OUTPUT.PUT_LINE('Object = '||l_tabowner||'.'||l_tabname||'.'||l_lobname);
-  DBMS_OUTPUT.PUT_LINE('Compression Type = DBMS_COMPRESSION.COMP_LOB_HIGH');  
-  DBMS_OUTPUT.PUT_LINE('Block count compressed = '|| l_blkcnt_cmp);
-  DBMS_OUTPUT.PUT_LINE('Block count uncompressed = '|| l_blkcnt_uncmp);
-  DBMS_OUTPUT.PUT_LINE('Row count per block compressed = '|| l_row_cmp);
-  DBMS_OUTPUT.PUT_LINE('Row count per block uncompressed = '|| l_row_uncmp);
-  DBMS_OUTPUT.PUT_LINE('Compression type = '|| l_cmptype_str);
-  DBMS_OUTPUT.PUT_LINE('Compression ratio = '|| l_cmp_ratio);
+    -- Display compression information for each compression type
+    DBMS_OUTPUT.PUT_LINE('Object = ' || l_tabowner || '.' || l_tabname || '.' || l_lobname);
+    DBMS_OUTPUT.PUT_LINE('Number of blocks used by the compressed sample of the object    :  ' || l_blkcnt_cmp);
+    DBMS_OUTPUT.PUT_LINE('Number of blocks used by the uncompressed sample of the object  :  ' || l_blkcnt_uncmp);
+    DBMS_OUTPUT.PUT_LINE('Number of LOBs actually sampled                                 :  ' || l_lobcnt);
+    DBMS_OUTPUT.PUT_LINE('Estimated Compression Ratio of Sample                           :  ' || l_cmp_ratio);
+    DBMS_OUTPUT.PUT_LINE('Compression ratio                                               :  ' ||l_blkcnt_uncmp/l_blkcnt_cmp||' to 1');
+    DBMS_OUTPUT.PUT_LINE('Compression Type                                                :  ' || l_comptype_str);
+  END LOOP;
 END;
 /
