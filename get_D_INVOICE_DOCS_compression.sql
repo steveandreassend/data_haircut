8a2017f0972de5d2597ab6bd0d7bff37c37bcbc0
steveandreassend <119098920+steveandreassend@users.noreply.github.com>
2024-01-31 17:30:17 +0100

    Update get_D_INVOICE_DOCS_compression.sql

--- get_D_INVOICE_DOCS_compression.sql
+++ get_D_INVOICE_DOCS_compression.sql
@@ -11,44 +11,96 @@ DECLARE
   l_cmp_ratio      NUMBER;
   l_comptype_str   VARCHAR2(32767);
   l_scratchtbsname varchar2(256) := 'USERS';
+  l_tabowner varchar2(256) := 'AAX2DMSSW';
+  l_tabname varchar2(256) := 'D_INVOICE_DOCS';
 
   l_numbers CONSTANT SYS.ODCINUMBERLIST := SYS.ODCINUMBERLIST(DBMS_COMPRESSION.COMP_ADVANCED,
-  /* Compression Advisor does not require Exadata to validate HCC ratios */
-DBMS_COMPRESSION.COMP_QUERY_LOW,
+    /* Compression Advisor does not require Exadata to validate HCC ratios */
+    DBMS_COMPRESSION.COMP_QUERY_LOW,
     DBMS_COMPRESSION.COMP_QUERY_HIGH,
     DBMS_COMPRESSION.COMP_ARCHIVE_LOW,
     DBMS_COMPRESSION.COMP_ARCHIVE_HIGH
   );
 
+  l_subpartition_name VARCHAR2(256);
+
 BEGIN
-/* find all tables larger than 1GB that are partitioned, and get the largest partition */
+  --there are 24 different document types, each stored in their own partition, and sub-partitioned by date range
+/*
+PARTITION_NAME
+------------------------------
+P_INV_DOC_APPBRS
+P_INV_DOC_APPFLN
+P_INV_DOC_APPSFC
+P_INV_DOC_CPO
+P_INV_DOC_DTCSV
+P_INV_DOC_DTS
+P_INV_DOC_DTSCUST
+P_INV_DOC_EFNBAG
+P_INV_DOC_EFNDE
+P_INV_DOC_FIBRS
+P_INV_DOC_FIFLN
+P_INV_DOC_FILHT
+P_INV_DOC_FISFC
+P_INV_DOC_MABAG
+P_INV_DOC_MADE
+P_INV_DOC_RS
+P_INV_DOC_SB
+P_INV_DOC_SF
+P_INV_DOC_SW
+P_INV_DOC_TDPOL
+P_INV_DOC_TFCSV
+P_INV_DOC_TSPOL
+P_INV_DOC_VSLHT
+P_INV_DOC_WDCSV
+
+24 rows selected. 
+*/
+
   FOR x IN (
-    SELECT owner, table_name, partition_name, segment_name, segment_type
+    SELECT DISTINCT partition_name
+    FROM dba_tab_partitions
+    WHERE TABLE_OWNER = l_tabowner
+    AND table_name = l_tabname
+    ORDER BY 1
+  ) LOOP
+
+    /* get the largest subpartition for the give partition_name */
+    SELECT partition_name INTO l_subpartition_name
     FROM (
-      SELECT a.owner, b.table_name, a.partition_name, a.segment_name, a.segment_type,
-      ROW_NUMBER() OVER (PARTITION BY b.table_name ORDER BY SUM(a.bytes) / (1024 * 1024 * 1024) DESC) AS rn
-      FROM dba_segments a, dba_tab_partitions b, dba_tablespaces c
-      WHERE a.segment_type IN ('TABLE PARTITION','TABLE SUBPARTITION')
+      SELECT a.owner,
+             b.table_name,
+             a.partition_name,
+             a.tablespace_name,
+             c.DEF_TAB_COMPRESSION,
+             c.COMPRESS_FOR,
+             c.BIGFILE,
+             c.STATUS,
+             a.segment_name,
+             a.segment_type,
+             ROW_NUMBER() OVER (PARTITION BY b.table_name ORDER BY SUM(a.bytes) / (1024 * 1024 * 1024) DESC) AS rn
+      FROM dba_segments a, dba_tab_subpartitions b, dba_tablespaces c
+      WHERE a.segment_type = 'TABLE SUBPARTITION'
       AND c.tablespace_name = a.tablespace_name
       AND a.owner = b.table_owner
+      AND b.table_name = l_tabname
       AND a.segment_name = b.table_name
-      AND a.partition_name = b.partition_name
-      AND b.table_name IN ('D_INVOICE_DOCS') /* specify your table names */
-      AND a.owner IN (SELECT username FROM dba_users where oracle_maintained = 'N') 
-      GROUP BY a.owner, b.table_name, a.partition_name, a.segment_name, a.segment_type
-      HAVING ROUND(SUM(a.bytes) / (1024 * 1024 * 1024), 2) >= 1
+      AND a.partition_name = b.subpartition_name
+      AND b.PARTITION_NAME = x.partition_name /* limit query of range based subpartitions to the given doc type partition */
+      and a.owner IN (SELECT username FROM dba_users where oracle_maintained = 'N')
+      GROUP BY a.owner, b.table_name, a.partition_name, a.tablespace_name, c.DEF_TAB_COMPRESSION, c.COMPRESS_FOR, c.BIGFILE, c.STATUS, a.segment_name, a.segment_type
     )
-    WHERE rn = 1
-  )
-  LOOP
-    DBMS_OUTPUT.PUT_LINE(chr(13)||chr(10)||'Object = ' || x.owner || '.' || x.table_name||'.'||x.partition_name);
+    WHERE rn = 1;
+
+    DBMS_OUTPUT.PUT_LINE(chr(13)||chr(10)||'Object = ' || l_tabowner || '.' || l_tabname || '.' || x.partition_name  ||'.' || l_subpartition_name ;
+
     FOR i IN 1..l_numbers.COUNT LOOP
       -- Loop through different compression types
       DBMS_COMPRESSION.GET_COMPRESSION_RATIO (
         scratchtbsname => l_scratchtbsname,
-        ownname        => x.owner,
-        objname        => x.table_name,
-        subobjname     => x.partition_name,
+        ownname        => l_tabowner,
+        objname        => l_tabname,
+        subobjname     => l_subpartition_name,
         comptype       => l_numbers(i),
         blkcnt_cmp     => l_blkcnt_cmp,
         blkcnt_uncmp   => l_blkcnt_uncmp,
@@ -73,6 +125,8 @@ BEGIN
 END;
 /
 
+/* WE ALREADY HAVE THE INDEX DATA */
+EXIT;
 /* alternate way to do all index types together
   
 DECLARE
