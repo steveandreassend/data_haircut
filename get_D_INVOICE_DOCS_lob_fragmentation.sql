10e67ac9d56152e13c1cf7212330a7f0382c4967
steveandreassend <119098920+steveandreassend@users.noreply.github.com>
2024-02-02 16:49:06 +0100

    Update get_D_INVOICE_DOCS_lob_fragmentation.sql

--- get_D_INVOICE_DOCS_lob_fragmentation.sql
+++ get_D_INVOICE_DOCS_lob_fragmentation.sql
@@ -79,6 +79,7 @@ DECLARE
   l_owner VARCHAR2(64) := 'AAX2DMSSW';
   l_segname VARCHAR2(64) := 'SYS_LOB0000277792C00008$$';
   l_segtype VARCHAR2(64) := 'LOB';
+  l_tabname varchar2(256) := 'D_INVOICE_DOCS';
   l_segment_size_blocks NUMBER;
   l_segment_size_bytes NUMBER;
   l_used_blocks NUMBER;
@@ -102,7 +103,16 @@ BEGIN
     raise_application_error(-20000,'Specify LOB, or LOB PARTITION if the target objected is a partitioned LOB column');
   END IF;
 
-  DBMS_SPACE.SPACE_USAGE(
+  FOR x IN (
+    SELECT DISTINCT partition_name
+    FROM dba_tab_partitions
+    WHERE TABLE_OWNER = l_tabowner
+    AND table_name = l_tabname
+    ORDER BY 1
+  ) LOOP
+
+  BEGIN
+    DBMS_SPACE.SPACE_USAGE(
       segment_owner => UPPER(l_owner),
       segment_name => UPPER(l_segname),
       segment_type => UPPER(l_segtype),
@@ -113,24 +123,32 @@ BEGIN
       expired_blocks => l_expired_blocks,
       expired_bytes => l_expired_bytes,
       unexpired_blocks => l_unexpired_blocks,
-      unexpired_bytes => l_unexpired_bytes
-  );
+      unexpired_bytes => l_unexpired_bytes,
+      partition_name => x.partition_name
+    );
 
-  l_unused_blocks := l_segment_size_blocks - (l_used_blocks + l_expired_blocks + l_unexpired_blocks);
-  l_unused_bytes := l_segment_size_bytes - (l_used_bytes + l_expired_bytes + l_unexpired_bytes);
-  l_non_data_blocks := l_unused_blocks + l_expired_blocks + l_unexpired_blocks;
-  l_non_data_bytes := l_unused_bytes + l_expired_bytes + l_unexpired_bytes;
+    l_unused_blocks := l_segment_size_blocks - (l_used_blocks + l_expired_blocks + l_unexpired_blocks);
+    l_unused_bytes := l_segment_size_bytes - (l_used_bytes + l_expired_bytes + l_unexpired_bytes);
+    l_non_data_blocks := l_unused_blocks + l_expired_blocks + l_unexpired_blocks;
+    l_non_data_bytes := l_unused_bytes + l_expired_bytes + l_unexpired_bytes;
 
-  DBMS_OUTPUT.ENABLE;
-  DBMS_OUTPUT.PUT_LINE(' Owner.Segment Namne    = '||UPPER(l_owner)||'.'||UPPER(l_segname));
-  DBMS_OUTPUT.PUT_LINE(' Segment Blocks/Bytes   = '||l_segment_size_blocks||' / '||l_segment_size_bytes);
-  DBMS_OUTPUT.PUT_LINE(' Unused Blocks/Bytes    = '||l_unused_blocks||' / '||l_unused_bytes);
-  DBMS_OUTPUT.PUT_LINE(' Used Blocks/Bytes      = '||l_used_blocks||' / '||l_used_bytes);
-  DBMS_OUTPUT.PUT_LINE(' Expired Blocks/Bytes   = '||l_expired_blocks||' / '||l_expired_bytes);
-  DBMS_OUTPUT.PUT_LINE(' Unexpired Blocks/Bytes = '||l_unexpired_blocks||' / '||l_unexpired_bytes);
-  DBMS_OUTPUT.PUT_LINE('===========================================================================');
-  DBMS_OUTPUT.PUT_LINE(' Non-Data Blocks/Bytes  = '||l_non_data_blocks||' / '||l_non_data_bytes);
+    DBMS_OUTPUT.ENABLE;
+    DBMS_OUTPUT.PUT_LINE(' Owner.Segment Name.Partition Name  = '||UPPER(l_owner)||'.'||UPPER(l_segname)||'.'||UPPER(x.partition_name));
+    DBMS_OUTPUT.PUT_LINE(' Segment Blocks/Bytes   = '||l_segment_size_blocks||' / '||l_segment_size_bytes);
+    DBMS_OUTPUT.PUT_LINE(' Unused Blocks/Bytes    = '||l_unused_blocks||' / '||l_unused_bytes);
+    DBMS_OUTPUT.PUT_LINE(' Used Blocks/Bytes      = '||l_used_blocks||' / '||l_used_bytes);
+    DBMS_OUTPUT.PUT_LINE(' Expired Blocks/Bytes   = '||l_expired_blocks||' / '||l_expired_bytes);
+    DBMS_OUTPUT.PUT_LINE(' Unexpired Blocks/Bytes = '||l_unexpired_blocks||' / '||l_unexpired_bytes);
+    DBMS_OUTPUT.PUT_LINE('===========================================================================');
+    DBMS_OUTPUT.PUT_LINE(' Non-Data Blocks/Bytes  = '||l_non_data_blocks||' / '||l_non_data_bytes);
+  EXCEPTION
+    WHEN OTHERS THEN
+      -- Handling exceptions
+      DBMS_OUTPUT.PUT_LINE('SQL Error Code: ' || SQLCODE);
+      DBMS_OUTPUT.PUT_LINE('SQL Error Message: ' || SQLERRM);
+    END;
 
+  END LOOP;
 END;
 /
 
